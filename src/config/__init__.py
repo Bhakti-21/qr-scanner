7d03646ebc6331e94733bbf028deda724c97a520
Bhakti <bhaktisurve21@gmail.com>
2020-08-02 20:58:53 +0530

    feat : added postgress config

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -5,15 +5,15 @@ class Config(object):
 
 
 class DevelopmentConfig(Config):
-    MEDIA_PATH = '/home/abhishek/Bhakti/media_files'
-    POSTGREHOST = 'localhost'
-    POSTGREPASSWORD = 'qr_user'
-    POSTGREUSER = 'qr_user'
+    MEDIA_PATH = '/qr_scan/media_files'
+    POSTGREHOST = 'ec2-54-236-146-234.compute-1.amazonaws.com'
+    POSTGREPASSWORD = '9682020be8c922bea72f66568698315a0b6ff8eaa59e9b0953f2179d2f60f903'
+    POSTGREUSER = 'wanturrslkhtcj'
     POSTGREPORT = '5432'
-    POSTGREDB = 'qr_db'
+    POSTGREDB = 'd3ddb6d1nmja01'
     ALLOWED_MIMETYPES = ['image/jpg', 'image/jpeg', 'image/png']
-    UPLOAD_FOLDER = '/home/abhishek/bhakti_projects/qr_scan/media'
-    CONNECTION_STRING = f'postgresql://qr_user:qr_user@localhost:5432/qr_db'
+    UPLOAD_FOLDER = '/qr_scan/media'
+    CONNECTION_STRING = f'postgres://wanturrslkhtcj:9682020be8c922bea72f66568698315a0b6ff8eaa59e9b0953f2179d2f60f903@ec2-54-236-146-234.compute-1.amazonaws.com:5432/d3ddb6d1nmja01'
 
 
 app_config = {
